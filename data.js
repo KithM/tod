ec5b189ba2713cf0ec00cb80cc41cfdfdf744b14
Kith M <malachite.akin@gmail.com>
2023-08-18 10:08:55 -0500

    Data

--- data.js
+++ data.js
@@ -1,21 +1,48 @@
 const campaignData = {
     characters: {
+        // Major Characters
         lich: {
             name: "The Lich (BBEG)",
-            description: "Once a revered Magi (Magister Eldryn), celebrated for his unparalleled wisdom and formidable power, he met a tragic demise..."
-            // ... more details
+            description: "Once a revered Magi (Magister Eldryn), celebrated for his unparalleled wisdom and formidable power, he met a tragic demise. In the aftermath of his downfall, the Undying Cult chose to reanimate him as a Lich, intending to unleash vengeance upon the lands. He now exists as an unwilling pawn, an escalating menace to the realm."
         },
         cult_leader: {
             name: "Cult Leader (Calix)",
-            description: "The charismatic and ruthless leader of the Undying Cult..."
-            // ... more details
+            description: "The charismatic and ruthless leader of the Undying Cult. Once a leading acolyte at the Temple of Oth'kahar, he left under the influence of the Eye’s visions. Intent on bringing Magister Eldryn back as a Lich, he believes it will restore the rightful glory of the Temple."
         },
         graham_cliffstone: {
             name: "Graham Cliffstone (G. or G.C.)",
-            description: "The enigmatic leader of the Covenant of the Black Hand..."
-            // ... more details
+            description: "The enigmatic leader of the Covenant of the Black Hand. Despite his infamy, Graham maintains a low profile. He's a strategic thinker and his actions are always calculated. While he may seem indifferent to the party at first, his interest may be piqued if they prove to be useful."
+        },
+
+        // Minor Characters
+        lady_adelaide: {
+            name: "Lady Adelaide",
+            description: "A regal figure of power in Summerhall, Lady Adelaide is a pragmatic leader who puts her city's interests above all. She is aware of the harsh realities of politics and isn't afraid to make tough decisions. She has tasked the players with their quest, promising ample reward."
+        },
+        lord_varis: {
+            name: "Lord Varis",
+            description: "A complex character, the Lord of Summerhall made the controversial decision to relocate the Thieves Guild to New Isle. While his intentions may seem murky, it’s clear that he's a shrewd character willing to use unconventional methods for what he believes is the greater good."
+        },
+        eamon: {
+            name: "Eamon",
+            description: "Living in solitude in the mountains, Eamon is a former member of the Vanguard of the Sun. He abandoned the guild when their lust for power overshadowed their mission for balance. He is a subtle source of information for the players."
+        },
+        korag: {
+            name: "Korag",
+            description: "The stern and battle-hardened leader of the Kavanash encampment. He is wary of outsiders, but respects strength and bravery. If the party can prove themselves, he may become a valuable ally."
+        },
+        grick: {
+            name: "Grick",
+            description: "An outcast Imp with an insatiable appetite for mischief and chaos. His non-hostile behavior is unusual for his kind, making him an intriguing character. Despite his comedic nature, he has his own backstory of rejection and loneliness. If the players pique his interest or show him kindness, he might offer them unexpected assistance or draw them into Imp politics."
+        },
+        thane: {
+            name: "Thane",
+            description: "Once a loyal student of Magister Eldryn, he played a significant role in Eldryn's downfall. Now, his loyalty to the Undying Cult has made him an adversary to those opposing the Lich's resurrection."
+        },
+        eye_of_othkahar: {
+            name: "Eye of Oth’kahar",
+            description: "After being released from the now-abandoned temple of Oth’kahar, the eye seeks its new purpose and follows the players in their journey, aiding them by revealing imagery."
         }
-        // ... other characters
-    },
+    }
     // ... other categories like factions, locations, etc.
 };
